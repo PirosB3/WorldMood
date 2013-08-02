0523abd58f0e394fe73e2d07fe0dcfcad50ec506
Daniel Pyrathon <pirosb3@gmail.com>
2013-08-03 00:33:29 +0200

    added params to it

--- data_miner.js
+++ data_miner.js
@@ -1,7 +1,22 @@
 var Twit = require('twit'),
 		redis = require('redis');
 
-TARGET = [':('];
+ARGS = {
+	positive: {
+		filter: ':)',
+		key: 'positive'
+	},
+	negative: {
+		filter: ':(',
+		key: 'negative'
+	}
+}
+
+var target = ARGS[process.argv[2]];
+if (!target) {
+	console.log("Not a valid sentiment");
+	process.exit(1);
+}
 
 var twit = new Twit({
     consumer_key:         'tIPFMyYowULdaxhXAUdw'
@@ -14,10 +29,10 @@ client.on("error", function (err) {
 		console.log("Error " + err);
 });
 
-var stream = twit.stream('statuses/filter', { track: TARGET, language: 'en' })
+var stream = twit.stream('statuses/filter', { track: target.filter, language: 'en' })
 stream.on('tweet', function (tweet) {
 	var text = tweet['text'];
 
-	client.sadd('sentiment-analysis:negative', text)
+	client.sadd('sentiment-analysis:' + target.key, text)
 	console.log(text);
 })
