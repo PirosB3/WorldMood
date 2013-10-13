4bb204a029bd0111dc0f61eb82b147050044786c
Daniel Pyrathon <pirosb3@gmail.com>
2013-10-13 22:51:25 +0100

    initial commit of svm version of classifier

---
+++ classifier-redis-svm.py
@@ -0,0 +1,151 @@
+import zmq
+import redis
+import functools
+import heapq
+import os
+from collections import defaultdict
+import re, math, collections, itertools
+import nltk, nltk.classify.util, nltk.metrics
+from nltk.classify import NaiveBayesClassifier
+from nltk.metrics import BigramAssocMeasures
+from nltk.probability import FreqDist, ConditionalFreqDist
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from formatting import FormatterPipeline
+import formatting
+from sklearn import svm
+from operator import add
+import numpy as np
+
+SENTIMENT_MAP = {
+    'pos': 0,
+    'neg': 1,
+    'neutral': 2
+}
+
+FORMATTER = FormatterPipeline(
+    formatting.make_lowercase,
+    formatting.strip_urls,
+    formatting.strip_hashtags,
+    formatting.strip_names,
+    formatting.replace_html_entities,
+    formatting.remove_repetitons,
+    functools.partial(
+        formatting.remove_noise,
+        stopwords = stopwords.words('english') + ['rt']
+    ),
+    functools.partial(
+        formatting.stem_words,
+        stemmer= nltk.stem.porter.PorterStemmer()
+    ),
+    functools.partial(
+        formatting.lemmatize_words,
+        lemmatizer= nltk.stem.wordnet.WordNetLemmatizer()
+    )
+)
+
+def get_features():
+
+    _filter_none = functools.partial(filter, lambda words: len(words))
+
+    def _format(sentence, sentiment):
+        formatted_sentence = FORMATTER.process(sentence)
+        return nltk.wordpunct_tokenize(formatted_sentence), sentiment
+
+    client = redis.Redis()
+    pos_sentences = list(client.smembers('sentiment-analysis-3:positive'))
+    neg_sentences = list(client.smembers('sentiment-analysis-3:negative'))
+    neutral_sentences = list(client.smembers('sentiment-analysis-4:neutral'))
+
+    pos_features = map(functools.partial(_format, sentiment='pos'), pos_sentences)
+    neg_features = map(functools.partial(_format, sentiment='neg'), neg_sentences)
+    neutral_features = map(functools.partial(_format, sentiment='neutral'), neutral_sentences)
+    return _filter_none(pos_features), _filter_none(neg_features), _filter_none(neutral_features)
+
+def get_most_informative_features(freq_dist, cond_freq_dist):
+    pos_word_count = cond_freq_dist['pos'].N()
+    neg_word_count = cond_freq_dist['neg'].N()
+    neutral_word_count = cond_freq_dist['neutral'].N()
+    total_word_count = pos_word_count + neg_word_count + neutral_word_count
+
+    result = []
+    for word, total_freq in freq_dist.iteritems():
+        pos_score = BigramAssocMeasures.chi_sq(cond_freq_dist['pos'][word], (
+            total_freq, pos_word_count), total_word_count)
+        neg_score = BigramAssocMeasures.chi_sq(cond_freq_dist['neg'][word], (
+            total_freq, neg_word_count), total_word_count)
+        neutral_score = BigramAssocMeasures.chi_sq(cond_freq_dist['neutral'][word], (
+            total_freq, neg_word_count), total_word_count)
+        result.append((pos_score + neg_score + neutral_score, word))
+    return [word for score, word in sorted(result, reverse=True)]
+
+def evaluate_classifier(valid_features, pos_features, neg_features, neutral_features):
+
+    def _extract_valid(feature):
+        res = {}
+        keys, sentiment = feature
+        for key in keys.keys():
+            if key in valid_features:
+                res[key] = True
+        return (res, sentiment)
+
+    pos_cutoff = len(pos_features) * 3/4
+    neg_cutoff = len(neg_features) * 3/4
+    neutral_cutoff = len(neutral_features) * 3/4
+    train_set = pos_features[:pos_cutoff] + neg_features[:neg_cutoff] + neutral_features[:neutral_cutoff]
+    test_set = pos_features[pos_cutoff:] + neg_features[neg_cutoff:] + neutral_features[neutral_cutoff:]
+    import ipdb; ipdb.set_trace()
+    
+    return accuracy, classifier
+
+def svmify(*feature_list):
+    words = []
+    phrases = []
+    sentiments = []
+    for phrase, sentiment in reduce(add, feature_list, []):
+        indexes = []
+        for word in phrase:
+            if word not in words:
+                words.append(word)
+            indexes.append(words.index(word))
+        phrases.append(indexes)
+        sentiments.append(SENTIMENT_MAP[sentiment])
+    return np.array(words), np.array(phrases), np.array(sentiments)
+
+def main():
+    # Fetch features from phrases
+    pos_features, neg_features, neutral_features = get_features()
+    words, phrases, sentiments = svmify(pos_features, neg_features, neutral_features)
+
+    cutoff = len(phrases) * 3/4
+
+    import ipdb; ipdb.set_trace()
+    clf = svm.SVC()
+    clf.fit(phrases[:cutoff], sentiments[:cutoff]) 
+    print clf.score(phrases[cutoff:], sentiments[cutoff:]) 
+
+
+
+
+    # loop and classify incoming data through ZMQ
+    context = zmq.Context.instance()
+    sock = context.socket(zmq.REP)
+    sock.connect('tcp://localhost:9999')
+    print "Listening to ZMQ"
+    while True:
+        message = sock.recv()
+
+        message_formatted = FORMATTER.process(message)
+        x = {}
+        for z in nltk.wordpunct_tokenize(message_formatted):
+            x[z] = True
+
+        classification = classifier.classify(x)
+        classification_prob = classifier.prob_classify(x).prob(classification)
+        if classification_prob > .8:
+            print "[%s]: %s" % (classification, message)
+        sock.send(classifier.classify(x))
+
+
+if __name__ == '__main__':
+    main()
