7150fc2c2d6c99b4c4283a853c203515612b25c9
Daniel Pyrathon <pirosb3@gmail.com>
2014-01-20 10:43:19 +0000

    color fixes and prettiness

--- web/public/javascripts/src/tweetView.js
+++ web/public/javascripts/src/tweetView.js
@@ -2,20 +2,22 @@ define(['marionette'], function() {
     return Backbone.Marionette.ItemView.extend({
         template: "#tweetViewTemplate",
         COLORS: {
-            'positive': _.template('rgba(217, 83, 79, <%= opacity %>)'),
-            'negative': _.template('rgba(92, 184, 92, <%= opacity %>)')
+            'negative': _.template('rgba(217, 83, 79, <%= opacity %>)'),
+            'positive': _.template('rgba(92, 184, 92, <%= opacity %>)')
         },
-        getOpacityFromAccuracy: function(n) {
-            return n * 0.01;
-        },
+	serializeData: function(){
+	    var attrs = Backbone.Marionette.ItemView.prototype.serializeData.call(this);
+	    attrs.trimmedAccuracy = this.model.getAccuracy().toString().substr(0, 5);
+	    return attrs;
+	},
         onRender: function() {
             this.$('img').attr('src', this.model.get('user').profile_image_url);
             this._setColorAndOpacity();
         },
         _setColorAndOpacity: function() {
             var colorTemplate = this.COLORS[this.model.get('prediction').result];
-            this.$el.css('background', colorTemplate({
-                opacity: this.getOpacityFromAccuracy(this.model.getAccuracy())
+            this.$('.panel-body').css('background', colorTemplate({
+                opacity: this.model.getAccuracy()
             }));
         }
     });
