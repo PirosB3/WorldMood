6de7dfae2b7066a4039292954a8b19840e40efdd
Daniel Pyrathon <pirosb3@gmail.com>
2014-01-30 12:24:30 +0000

    added init scripts

---
+++ config/run_classifier.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/Users/danielpiros/Documents/Projects/classifier/bin/python /Users/danielpiros/Documents/Projects/classifier/spinup_classifier.py --path /Users/danielpiros/Documents/SerializedClassifiers/sentiment-analysis-15000-3000-Maxent/
---
+++ config/run_core.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/bin/node /Users/danielpiros/Documents/Projects/classifier/web/app.js
